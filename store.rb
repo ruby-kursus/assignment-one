58d6fc401ba9e6bf1cb7ee62e425d854bcc1c54c
irhete <irene.teinemaa@gmail.com>
2013-03-13 19:57:04 +0200

    all tests, but unique_items not correct format

--- store.rb
+++ store.rb
@@ -4,38 +4,109 @@ require 'json'
 
 class Store
 @@items
+attr_accessor :total_sale
+
+  def initialize
+    @total_sale = 0
+  end
+
   def import_items(file_name)
     json = File.read(file_name)
     @@items = JSON.parse(json, :symbolize_names => true)
   end
+
   def search(keywords)
     searchable_items = @@items
     keywords.each do |kw_key, kw_value|
-      if kw_key == :available && kw_value == true
-          searchable_items = searchable_items.select{|item| item[:in_store] > 0}
-      elsif kw_key == :available
-        searchable_items = searchable_items.select{|item| item[:in_store] <= 0}
+      if kw_key == :available
+        searchable_items = searchable_items.select{|item| item.available?}
       else
-        searchable_items = searchable_items.select{|item| item[kw_key].downcase == kw_value.to_s.downcase}
+        searchable_items = searchable_items.select{|item| item[kw_key].to_s.downcase == kw_value.to_s.downcase}
       end
     end
     return searchable_items
   end
-end
 
-
-
-
-class Cart
-@@store
-  def initialize(store)
-   @@store = store
+  def items_sorted_by(attribute, order)
+    if order == :asc
+      return @@items.sort {|x,y| x[attribute] <=> y[attribute]}
+    else
+      return @@items.sort {|x,y| y[attribute] <=> x[attribute]}
+    end
   end
+
+  def categories
+    return @@items.map{ |item| item[:category] }.flatten.uniq
+  end
+
+  def unique_articles_in_category(category)
+    articles = []
+    @@items.select {|item| item[:category] == category}.map{ |item| item[:name] }.flatten.uniq.sort
+  end
+
+
+  class Cart
+    attr_accessor :store
+    attr_accessor :items
+
+    def initialize(store)
+      @store = store
+      @items = []
+    end
+
+    def total_cost
+      if total_items == 0
+        return 0.0
+      else
+        return @items.map{|item| item[:price] }.flatten.inject{|sum,x| sum + x }*100.round / 100.0
+      end
+    end
+
+    def add_item(item, quantity = 1)
+      if quantity > item.in_store
+        quantity = item.in_store
+      end
+      quantity.times do 
+        if item.available?     
+          @items << item
+        end
+      end
+    end
+
+    def total_items
+      return @items.size
+    end
+
+    def unique_items
+      return @items.uniq.first.to_a
+    end
+
+    def checkout!
+      @items.each do |item|
+        item[:in_store] -= 1
+      end
+      @store.total_sale += total_cost
+    end
+  end
+
 end
+
+
+
+
+
 
 
 class Hash
   def method_missing(n)
-    self[n.to_s.to_sym]
+    self[n.to_sym]
+  end
+
+  def available?
+    if self.in_store > 0
+      return true
+    else
+      return false
+    end
   end
 end
--- test_store.rb
+++ test_store.rb
@@ -23,23 +23,23 @@ class TestStore < MiniTest::Unit::TestCase
   def test_search_with_availability_flag
     assert_equal 'A lamp', @store.search(:color => 'green', :category => 'furniture', :available => true).first.name
   end
-=begin   
+  
   def test_sort_items_by_price_cheapest_first
     assert_equal 1.99, @store.items_sorted_by(:price, :asc).first.price
   end
-  
+ 
   def test_sort_items_by_in_stock_cheapest_last
     assert_equal 399.00, @store.items_sorted_by(:price, :desc).first.price
   end
-  
+
   def test_categories_list
     assert_equal ['Clothing', 'Furniture', 'Tools'], @store.categories
   end
-  
+ 
   def test_unique_items_in_category
     assert_equal ['Jeans', 'T-shirt'], @store.unique_articles_in_category('Clothing') 
   end
-  
+
   def test_cart_initialization
     assert_equal @store, @cart.store
     assert_equal [], @cart.items
@@ -66,7 +66,7 @@ class TestStore < MiniTest::Unit::TestCase
     assert_equal 5, @cart.total_items
     assert_equal Array(@default_item), @cart.unique_items
   end
-  
+ 
   def test_add_item_multiple_items_into_cart
     n = 2
     n.times { @cart.add_item(@default_item) }
@@ -74,7 +74,7 @@ class TestStore < MiniTest::Unit::TestCase
     assert_equal n + 1, @cart.total_items
     assert_equal 11.97, @cart.total_cost
   end
-  
+    
   def test_out_of_stock_items_cant_be_added_to_cart
     out_of_stock_item = @store.search(:in_store => 0).first
     @cart.add_item(out_of_stock_item)
@@ -94,16 +94,14 @@ class TestStore < MiniTest::Unit::TestCase
     @cart.checkout!
     assert_equal previously_in_store - 1, @default_item.in_store
   end
-  
+ 
   def test_initial_total_sale
     assert_equal 0, @store.total_sale
-  end
-  
+  end  
   def test_checkout_to_increase_total_sales
     n = 5
     @cart.add_item(@default_item, n)
     @cart.checkout!
     assert_equal @store.total_sale, (@default_item.price * n).round(2)
   end
-=end
 end
